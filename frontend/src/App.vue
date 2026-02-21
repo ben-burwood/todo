9a0f90fa800e3ca98ddd3be3eb9e0eba5e9b467f
Ben Burwood <77846694+ben-burwood@users.noreply.github.com>
2026-02-21 00:20:08 +0000

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -116,9 +116,6 @@ async function deleteTodo(uuid: string) {
     }
     try {
         const res = await fetch(`${SERVER_URL}/todos/${uuid}`, { method: "DELETE" });
-        if (!res.ok) {
-            throw new Error(`Failed to delete todo: ${res.status}`);
-        }
         await fetchTodos();
     } catch (error: any) {
         errorMessage.value = `Error: Deleting Todo : ${error.message}`;
