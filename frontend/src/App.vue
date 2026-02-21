9f78e53f6bab5c07aba271a6b13e7e9e600706b6
Ben Burwood <77846694+ben-burwood@users.noreply.github.com>
2026-02-21 00:14:18 +0000

    Remove extraneous response.ok Check

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -113,9 +113,6 @@ async function toggleCompleted(uuid: string) {
 async function deleteTodo(uuid: string) {
     try {
         const res = await fetch(`${SERVER_URL}/todos/${uuid}`, { method: "DELETE" });
-        if (!res.ok) {
-            throw new Error(`Failed to delete todo: ${res.status}`);
-        }
         await fetchTodos();
     } catch (error: any) {
         errorMessage.value = `Error: Deleting Todo : ${error.message}`;
