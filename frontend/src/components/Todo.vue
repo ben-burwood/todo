e6a6afb41be571124df64dadb2c97a423edecbdc
Ben Burwood <77846694+ben-burwood@users.noreply.github.com>
2026-02-21 10:39:57 +0000

    Remove EDIT_ENABLED condition from template

--- frontend/src/components/Todo.vue
+++ frontend/src/components/Todo.vue
@@ -36,7 +36,7 @@
                         />
                     </svg>
                 </button>
-                <template v-if="isEditing && EDIT_ENABLED">
+                <template v-if="isEditing">
                     <button class="btn btn-sm btn-success" @click="saveEdit" :disabled="editedTodo.trim() === ''">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
