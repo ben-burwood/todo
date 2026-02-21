5b146594e9063368b00333285bc4035469178934
Ben Burwood <77846694+ben-burwood@users.noreply.github.com>
2026-02-21 23:05:46 +0000

    Disable Delete Feature

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,6 @@ import './style.css';
 
 export const SERVER_URL = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
 export const EDIT_ENABLED = true;
-export const DELETE_ENABLED = true;
+export const DELETE_ENABLED = false;
 
 createApp(App).mount('#app')
