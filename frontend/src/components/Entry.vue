b9fac3b66a2feea1aec9a7f7f2e129c7c0b7c098
Ben Burwood <77846694+ben-burwood@users.noreply.github.com>
2026-03-09 20:47:49 +0000

    Rename todo variable to newTodo in Entry.vue

--- frontend/src/components/Entry.vue
+++ frontend/src/components/Entry.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="join">
-        <input type="text" class="input w-full join-item" placeholder="New todo..." v-model="todo" @keyup.enter="addTodo" />
-        <button class="btn btn-accent join-item" @click="addTodo" :disabled="todo.trim() === ''">Add</button>
+        <input type="text" class="input w-full join-item" placeholder="New todo..." v-model="newTodo" @keyup.enter="addTodo" />
+        <button class="btn btn-accent join-item" @click="addTodo" :disabled="newTodo.trim() === ''">Add</button>
     </div>
 </template>
 
@@ -10,11 +10,13 @@ import { ref } from "vue";
 
 const emit = defineEmits(["add"]);
 
-const todo = ref("");
+const newTodo = ref("");
 
 function addTodo() {
-    if (todo.value.trim() === "") return;
-    emit("add", todo.value);
+    const content = newTodo.value.trim();
+    if (!content) return;
+
+    emit("add", content);
     todo.value = "";
 }
 </script>
